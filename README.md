858f6e1f7bfb387c03d5084ca879ecece0579275
alextanhongpin <alextan@seekasia.com>
2017-11-13 08:31:56 +0800

    chore: initial commit
    add dependencies, create README and .gitignore

---
+++ README.md
@@ -0,0 +1,6 @@
+# node-opentracing
+
+
+## Reference
+
+1. [OpenTracing](https://blog.risingstack.com/distributed-tracing-opentracing-node-js/)
---
+++ index.js
@@ -0,0 +1,28 @@
+// const { initTracer, ConstSampler } = require('jaeger-client')
+
+// const config = {
+//   serviceName: 'some-service',
+//   disable: false,
+//   sampler: {
+//     type: ConstSampler(),
+//     param: 0,
+//     host: '',
+//     port: 0,
+//     refreshIntervalMs: 5000
+//   },
+//   reporter: {
+//     logSpans: false,
+//     agentHost: '',
+//     agentPort: 0,
+//     flushIntervalMs: 5000
+//   }
+// }
+// const options = {
+//   tags: {
+//     'some-service.version': '1.0.0'
+//   },
+//   metrics: metrics,
+//   logger: logger
+// }
+
+// const tracer = initTracer(config, options)
